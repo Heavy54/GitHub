a48c9b1c6328ce441a8bd6a74df04b3ae73d1d16
Heavy54 <mathieu.jo@sfr.fr>
2014-03-10 14:13:27 +0100

    setFullPath() + getFullPath()
    SetFullPath() created and finished
    + GetFullPath() created and finished
    + Modifications to be in line with conventions
    + More Comments
    + Doc

--- MultiViewTrackerApplication/MultiViewTrackerApplication/MultiViewTrackerApplication.cpp
+++ MultiViewTrackerApplication/MultiViewTrackerApplication/MultiViewTrackerApplication.cpp
@@ -1,18 +1,35 @@
+//********************************
+// MultiViewTracker Application main file
+//********************************
+
 #include "highgui.h"
 #include <iostream>
-
-using namespace std;
-using namespace cv;
+#include "Loader.h"
 
 int main(int argc, char* argv[])
 {
-	// A donner en parametre
-	const string path = "X:/MultiViewTracker/Calibrations/test/Foot001DR/Images";
-	const string extension = ".JPG";
+	// Will must be given in parameter 
+	// Path to sequence
+	const std::string path = "X:/MultiViewTracker/Calibrations/test/Foot001DR/Images"; 
+	const std::string extension = ".JPG";
+	// Number of digits in filename
 	const unsigned int num_digits = 4;
+	const std::string middle_char = "_";
 
-	/***** Test imread et show image
-	*/Mat image = imread("X:/MultiViewTracker/Calibrations/test/Foot001DR/Images/Camera0_0000.JPG");
+	/***** Test imread and show image
+	Mat image = imread("X:/MultiViewTracker/Calibrations/test/Foot001DR/Images/Camera0_0000.JPG");
 	imshow("Test", image);
-	waitKey();
+	waitKey();*/
+
+	Loader loader(path, extension,  num_digits, middle_char);
+	
+	// Setting of full path for the Loader
+	/* For now cam_nr et frame _nr initialized manually
+	First zero: camera number
+	Second zero : frame number*/
+	loader.setFullPath(0,0);
+	std::string full_path = loader.getFullPath();
+
+	std::cout << full_path << std::endl;
+	system("pause");
 }
